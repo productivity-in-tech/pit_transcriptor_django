d35174b056cdc00a0f334e814b3ca72cfc6fc5b3
Jay Miller <kjaymiller@gmail.com>
2020-01-10 16:48:17 +0000

    add rss_update_check

--- projects/apps.py
+++ projects/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
 
-
 class ProjectsConfig(AppConfig):
     name = 'projects'
+
+    def ready(self):
+        from django_q.tasks import schedule
+
+        schedule(
+                'projects.tasks.check_for_new_files',
+                schedule_type='H',
+                minutes=1,
+                )
--- projects/tasks.py
+++ projects/tasks.py
@@ -1,4 +1,4 @@
-from django_q.models import Schedule
+from django.apps import AppConfig
 from django.utils import timezone
 
 from .models import Project
@@ -8,9 +8,10 @@ from .helpers import transcription_get_or_create
 def check_for_new_files():
     updating_projects = Project.objects.filter(rss_updates=True)
 
-    for project in projects:
+    for project in updating_projects:
         feed_data =  get_feed_data()
 
         for item in feed_data:
             transcription_get_or_create(feed_item, project=project)
-            
+
+
